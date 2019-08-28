aee44dd9708293112221acd835ae36f5991c9398
AustinBCole <austin.cole.chileno@gmail.com>
2019-08-28 12:05:51 -0600

    settingsView now animates to top of screen.

--- MexicanTreasure/MexicanTreasure/HomeViewController.swift
+++ MexicanTreasure/MexicanTreasure/HomeViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var settingsViewController = SettingsViewController()
+    @IBOutlet weak var settingsView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,62 @@ class HomeViewController: UIViewController {
     @IBAction func restartGameButtonTapped(_ sender: Any) {
         StoryTree.shared.resetStoryTree()
     }
-
+    @IBAction func settingsButtonTapped(_ sender: Any) {
+        animateSettingsView()
+        
+    }
+    private func animateSettingsView() {
+        let animationDuration = 3.0
+        let position: CGFloat = 100.0
+        UIView.animateKeyframes(withDuration: animationDuration, delay: 0.0, options: .calculationModeLinear, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/3, animations: {
+                self.settingsView.frame = CGRect(x: self.settingsView.frame.midX, y: 20, width: 100, height: 100)
+                self.deactivateConstraints(targetedView: self.settingsView)
+                self.setNewSettingsViewConstraints()
+            })
+        }, completion: { completed in
+            if completed {
+                
+            }
+        })
+    }
+    private func deactivateConstraints(targetedView: UIView) {
+        for constraint in targetedView.constraints {
+            constraint.isActive = false
+        }
+    }
+    private func setNewSettingsViewConstraints() {
+        let bottomConstraint = NSLayoutConstraint(item: settingsView,
+                                               attribute: .bottom,
+                                               relatedBy: .equal,
+                                               toItem: view,
+                                               attribute: .bottom,
+                                               multiplier: 1.0,
+                                               constant: 20)
+        let widthConstraint = NSLayoutConstraint(item: settingsView,
+                                                   attribute: .width,
+                                                   relatedBy: .equal,
+                                                   toItem: nil,
+                                                   attribute: .width,
+                                                   multiplier: 1.0,
+                                                   constant: 100)
+        let heightConstraint = NSLayoutConstraint(item: settingsView,
+                                                    attribute: .height,
+                                                    relatedBy: .equal,
+                                                    toItem: nil,
+                                                    attribute: .height,
+                                                    multiplier: 1.0,
+                                                    constant: 100)
+        let centerXConstraint = NSLayoutConstraint(item: settingsView,
+                                                  attribute: .centerX,
+                                                  relatedBy: .equal,
+                                                  toItem: view,
+                                                  attribute: .centerX,
+                                                  multiplier: 1.0,
+                                                  constant: 0)
+        NSLayoutConstraint.activate([bottomConstraint, widthConstraint, heightConstraint, centerXConstraint])
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ MexicanTreasure/MexicanTreasure/SettingsViewController.swift
@@ -0,0 +1,82 @@
+//
+//  SettingsViewController.swift
+//  MexicanTreasure
+//
+//  Created by Austin Cole on 8/27/19.
+//  Copyright © 2019 Austin Cole. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    private let settingsLabel = UILabel()
+    private let changeFontSizeButton = UIButton()
+    private let darkModeSwitch = UISwitch()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        createSubviews()
+    }
+
+    private func createSubviews() {
+        formatSettingsLabel()
+        createChangeFontSizeButton()
+        createDarkModeSwitch()
+    }
+    private func formatSettingsLabel() {
+        settingsLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(settingsLabel)
+        settingsLabel.textColor = .green
+        settingsLabel.text = "Settings"
+        
+        let topConstraint = NSLayoutConstraint(item: settingsLabel,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: view,
+                                               attribute: .top,
+                                               multiplier: 1.0,
+                                               constant: 60)
+        let leadingConstraint = NSLayoutConstraint(item: settingsLabel,
+                                                   attribute: .leading,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .leading,
+                                                   multiplier: 1.0,
+                                                   constant: 0)
+        let trailingConstraint = NSLayoutConstraint(item: settingsLabel,
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: view,
+                                                    attribute: .trailing,
+                                                    multiplier: 1.0,
+                                                    constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: settingsLabel,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: view,
+                                                  attribute: .top,
+                                                  multiplier: 1.0,
+                                                  constant: 80)
+        
+        NSLayoutConstraint.activate([topConstraint, leadingConstraint, trailingConstraint])
+    }
+    private func createChangeFontSizeButton() {
+        
+    }
+    private func createDarkModeSwitch() {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
