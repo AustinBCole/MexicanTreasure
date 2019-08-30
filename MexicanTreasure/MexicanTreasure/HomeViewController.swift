9edab2e115b097073ddce1e9998cb9775aaed332
AustinBCole <austin.cole.chileno@gmail.com>
2019-08-30 16:40:40 -0600

    Animation changed to 2 seconds, changed to end up in middle of page, buttons still work. Style button and label font.

--- MexicanTreasure/MexicanTreasure/HomeViewController.swift
+++ MexicanTreasure/MexicanTreasure/HomeViewController.swift
@@ -8,20 +8,23 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
     private var settingsViewController = SettingsViewController()
-    @IBOutlet weak var settingsStackView: UIView!
-    @IBOutlet weak var cancelButton: UIButton!
-    @IBOutlet weak var settingsView: UIView!
+    //    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var settingsTableView: UITableView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "mexican_treasure_home_resized_smaller")!)
-        settingsView.layer.cornerRadius = 10
-        cancelButton.layer.cornerRadius = 10
+        settingsTableView.layer.cornerRadius = 10
+        //        cancelButton.layer.cornerRadius = 10
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+        
     }
     
     @IBAction func restartGameButtonTapped(_ sender: Any) {
@@ -32,11 +35,11 @@ class HomeViewController: UIViewController {
         
     }
     private func animateSettingsView() {
-        let animationDuration = 3.0
+        let animationDuration = 2.0
         UIView.animateKeyframes(withDuration: animationDuration, delay: 0.0, options: .calculationModeLinear, animations: {
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/3, animations: {
-                self.settingsStackView.frame = CGRect(x: 0, y: self.view.frame.midY + self.view.frame.height * 0.2, width: 200, height: 150)
-
+                self.settingsTableView.frame = CGRect(x: self.view.frame.midX - 125, y: self.view.frame.midY + self.view.frame.height * 0.2, width: 250, height: 140)
+                
             })
         }, completion: { completed in
             if completed {
@@ -49,14 +52,41 @@ class HomeViewController: UIViewController {
             constraint.isActive = false
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! SettingsTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.formatSettingsLabel()
+        default:
+            cell.formatButton(index: indexPath.row)
+        }
+        cell.contentMode = .center
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row != 2 {
+            cell.separatorInset = UIEdgeInsets.zero
+            cell.layoutMargins = UIEdgeInsets.zero
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ MexicanTreasure/MexicanTreasure/SettingsTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsTableViewCell.swift
+//  MexicanTreasure
+//
+//  Created by Austin Cole on 8/30/19.
+//  Copyright © 2019 Austin Cole. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+    
+    func formatSettingsLabel() {
+        let settingsLabel = UILabel()
+        self.addSubview(settingsLabel)
+        settingsLabel.frame = CGRect(x: self.bounds.midX - 34, y: 0, width: 100, height: 50)
+        settingsLabel.text = "Settings"
+        settingsLabel.textColor = .gray
+        settingsLabel.font = .boldSystemFont(ofSize: 14)
+    }
+    func formatButton(index: Int) {
+        let button = UIButton(type: .system)
+        self.contentView.addSubview(button)
+        button.frame = CGRect(x: self.bounds.midX - 100, y: 0, width: 200, height: 50)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        switch index {
+        case 1:
+            button.setTitle("Choose Text Color", for: .normal)
+            
+        default:
+            button.setTitle("Choose Text Size", for: .normal)
+        }
+    }
+}
