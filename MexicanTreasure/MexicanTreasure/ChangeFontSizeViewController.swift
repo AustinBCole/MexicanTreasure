a8725e0e381cccc688fb8abc122076d797fc7c41
AustinBCole <austin.cole.chileno@gmail.com>
2019-09-04 16:14:56 -0600

    Add initial files for dark mode and changing font size.

---
+++ MexicanTreasure/MexicanTreasure/ChangeFontSizeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangeFontSizeViewController.swift
+//  MexicanTreasure
+//
+//  Created by Austin Cole on 9/4/19.
+//  Copyright © 2019 Austin Cole. All rights reserved.
+//
+
+import UIKit
+
+class ChangeFontSizeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
